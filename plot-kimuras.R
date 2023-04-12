8308ef8941b2c0a0b15203e9e5f7e0ba700cb600
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2023-04-12 08:53:13 +0200

    Add files via upload

--- plot-kimuras.R
+++ plot-kimuras.R
@@ -1,17 +1,20 @@
-library(kimura)
-
-nbin = 10
-x = (0:nbin)/nbin
-results = data.frame()
-for(nb in 1:14) {
-  pdf = dkimura(x, 0.5, 1-1/nb)
-  pdf[2:nbin] = pdf[2:nbin]*(1/(nbin-1))
-  results = rbind(results, data.frame(nb=nb,x=x,pdf=pdf))
+# transform effective cell division counts, given an old and a new population size
+# simulations are run with n=50. we might want, for example, n=200 for mitos or n=10 for plastids
+mytrans = function(n, ne.orig=50, ne.new=100) {
+  return(n*log(1-1/ne.orig)/log(1-1/ne.new))
 }
-png("kimura-dists.png", width=1000, height=1000)
-ggplot(results, aes(x=x,y=pdf)) + geom_col() + facet_wrap(~nb) + theme_void()
+
+# read in pre-existing PDF database (make sure simulation code has been run to provide this file)
+df = read.csv("dbtrj-50-100.csv")
+png("kimuras-mt.png", width=600, height=400)
+# plot distributions relevant for MT behaviour
+ggplot(df[df$h0 == 0.5 & df$n %in% c(4,6,8,12,25,50,80),], aes(x=h,y=Ph)) + 
+  geom_col(width=0.05) + facet_wrap(~ n, scales="free") + theme_void()
 dev.off()
 
-
-df = read.csv("dbtrj-50-100.csv")
-ggplot(df[df$h0 == 0.5 & df$n %in% c(100,1,2,5),], aes(x=h,y=Ph)) + geom_col() + facet_wrap(~ n)
+mytrans(c(1.5, 1, 0.5, 18), ne.orig=7, ne.new=50)
+png("kimuras-pt.png", width=400, height=400)
+# plot distributions relevant for PT behaviour
+ggplot(df[df$h0 == 0.5 & df$n %in% c(4,8,12,137),], aes(x=h,y=Ph)) + 
+  geom_col(width=0.05) + facet_wrap(~ n, scales="free") + theme_void()
+dev.off()
